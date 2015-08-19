9044f3aa6ab1b61b495ba8469f5ccfea66935f4b
yoshimalucky <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2015-08-19 17:15:22 +0900

    Added some comments in vel_publisher.py

--- scripts/vel_publisher.py
+++ scripts/vel_publisher.py
@@ -4,9 +4,10 @@ from geometry_msgs.msg import Twist
 
 rospy.init_node('vel_publisher')
 pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
+# rate = rospy.Rate(30) # 30Hz
 while not rospy.is_shutdown():
     vel = Twist()
-    direction = raw_input('f: forward, b: backward, l: left, r: right > ')
+    direction = raw_input('f: forward, b: backward, l: left, r: right > ') # wait until pressing Enter
     if 'f' in direction:
         vel.linear.x = 0.5
     if 'b' in direction:
@@ -19,3 +20,4 @@ while not rospy.is_shutdown():
         break
     print vel
     pub.publish(vel)
+    # rate.sleep()
